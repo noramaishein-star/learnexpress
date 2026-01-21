c6050f3f0902b116d316615ea5d01eca0bb671c5
Nora Mai Shein <noramai.shein@gmail.com>
2026-01-21 10:52:59 +0200

    add nunjucks

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import nunjucks from 'nunjucks';
 const app = express()
 const port = 3000
 
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello VM24!!!!!')
+  res.render('index.njk')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.njk')
+})
+
+
+app.get('/contact', (req, res) => {
+  res.render('contact.njk')
 })
 
 app.listen(port, () => {
