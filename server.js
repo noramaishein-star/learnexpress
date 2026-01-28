3bfc72f1fa48e5e5b1e23d7cee5390c76f7db21a
Nora Mai Shein <noramai.shein@gmail.com>
2026-01-28 09:23:24 +0200

    add variables example

--- server.js
+++ server.js
@@ -9,18 +9,27 @@ nunjucks.configure('views', {
 });
 
 app.get('/', (req, res) => {
-  res.render('index.njk')
-})
+  let name = 'Nora Mai';
+  let age = 17;
+  let fruits = [
+    'Mango',
+    'Pear',
+    'Orange',
+    'Coconut',
+    'Melon',
+  ]
+  res.render('index.njk', {name, age, fruits});
+});
 
 app.get('/about', (req, res) => {
   res.render('about.njk')
-})
+});
 
 
 app.get('/contact', (req, res) => {
   res.render('contact.njk')
-})
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
