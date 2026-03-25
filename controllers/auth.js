8fa61e2fa01752902c19b9a4b4583a94b9fdb1a2
Nora Mai Shein <noramai.shein@gmail.com>
2026-03-25 10:15:33 +0200

    Add bcrypt hashing to passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { prisma } from "../lib/prisma.js";
+import bcrypt from "bcryptjs";
+
 const router = express.Router();
 
 router.get("/register", async (req, res) => {
@@ -11,11 +13,12 @@ router.post("/register", async (req, res) => {
         where: { email: req.body.email },
     });
     if (!user && req.body.password === req.body.password_confirm) {
+        const salt = bcrypt. genSaltSync(12);
         await prisma.user.create({
             data: {
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
+                password: bcrypt.hashSync(req.body.password, salt),
             },
         });
         res.redirect("/login");
@@ -32,7 +35,7 @@ router.post("/login", async (req, res) => {
     let user = await prisma.user.findUnique({
         where: { email: req.body.email },
     });
-    if (user && user.password === req.body.password) {
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
         req.session.userID = user.id;
         res.redirect("/cats");
     } else {
