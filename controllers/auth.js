15673d119990066bd9b49765a5b9c7ed14e8934e
Nora Mai Shein <noramai.shein@gmail.com>
2026-03-25 08:52:01 +0200

    Add logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,4 +40,9 @@ router.post("/login", async (req, res) => {
     }
 });
 
+router.get("/logout", async (req, res) => {
+    delete req.session.userID;
+    res.redirect('/');
+});
+
 export default router;
